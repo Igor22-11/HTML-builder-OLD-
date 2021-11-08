88824ca41f3977ad75194da8f7386cde9629a9b0
Igor22-11 <igorbespalov89@gmail.com>
2021-11-08 10:38:40 +0300

    init:

---
+++ 02-write-file/index.js
@@ -0,0 +1,16 @@
+let path = require('path');
+let fs = require('fs');
+let writeStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+let { stdout, stdin, exit } = require('process');
+stdout.write('Введите текст:');
+stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    endFunc();
+  }
+  writeStream.write(data);
+});
+process.on('SIGINT', endFunc);
+function endFunc() {
+  stdout.write('До свидания!');
+  exit();
+};
